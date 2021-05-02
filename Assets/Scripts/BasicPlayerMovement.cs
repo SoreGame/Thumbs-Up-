6367b65c51b66471c166534a0dea1c8f0b963173
SoreGame <egorsol@bk.ru>
2021-05-02 21:56:42 +0500

    Player
    Добавил игрока + скрипт базового передвижения

---
+++ Assets/Scripts/BasicPlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    public bool lookRight = true;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (!lookRight && moveInput.x > 0)
+        {
+            Flip();
+        }
+        if (lookRight && moveInput.x < 0)
+        {
+            Flip();
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        lookRight = !lookRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
